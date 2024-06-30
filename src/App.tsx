0b3d38265a06ae53a3bfb807da63760be2282d9c
sabari-qwerty <sabarigithub@gmail.com>
2024-06-30 15:28:31 +0530

    showing data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { useEffect, useState } from "react";
 
+interface messageData {
+  avatar?: string;
+  name?: string;
+  reviewText?: string;
+  rating?: string | number;
+  source?: string;
+  url?: string;
+  timeStamp?: string;
+}
+
 export default function App() {
-  const [message, setMessage] = useState();
+  const [message, setMessage] = useState<messageData[]>();
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
@@ -10,9 +20,7 @@ export default function App() {
       console.log(data);
 
       if (data.key === "gz-message") {
-        console.log(data.data);
-
-        setMessage(data.data);
+        setMessage(data.data as messageData[]);
       }
     };
 
@@ -23,9 +31,22 @@ export default function App() {
     };
   }, []);
 
+  if (!message) return <div>loading</div>;
+
   return (
-    <div>
-      <p id="message">{message}</p>
+    <div className="flex flex-col overflow-scroll space-y-4">
+      {message.map((data, key) => (
+        <div key={key}>
+          <div>avatar: {data.avatar}</div>
+          <div>name: {data.name}</div>
+
+          <div>text: {data.reviewText}</div>
+          <div>rating: {data.rating}</div>
+          <div>source: {data.source}</div>
+          <div>url: {data.url}</div>
+          <div>timeStamp: {data.timeStamp}</div>
+        </div>
+      ))}
     </div>
   );
 }
